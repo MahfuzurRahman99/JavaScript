2828437e3180a0a00b1ad37cb231f594901389d2
MahfuzurRahman99 <mahfuzz999@gmail.com>
2024-01-03 12:31:54 +0600

    task 8

---
+++ Task 8/index.js
@@ -0,0 +1,21 @@
+function add(x, y) {
+    result = x + y;
+    return result;
+}
+function sub(x, y) {
+    result = x - y;
+    return result;
+}
+function div(x, y) {
+    result = x / y;
+    return result;
+}
+function mul(x, y) {
+    result = x + y;
+    return result;
+}
+
+document.write(add(5,6) + '<br/>');
+document.write(sub(5,6) + '<br/>');
+document.write(div(5,6) + '<br/>');
+document.write(mul(5,6) + '<br/>');
